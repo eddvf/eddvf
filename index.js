19c596b662fbf7ba6d598e24fd8067d3daaedeeb
eddvf <endrit.vorfaj@hotmail.com>
2024-04-04 09:00:31 +0200

    resp

--- index.js
+++ index.js
@@ -2,3 +2,24 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const brandLogo = document.querySelector('.brand-logo');
+    
+    // Function to adjust brand logo text based on screen width
+    function adjustBrandLogoText() {
+        if (window.innerWidth <= 600) {
+            // Shorter text for smaller screens
+            brandLogo.innerHTML = '@EV:~$ <span class="blinking-cursor"> | </span>';
+        } else {
+            // Full text for larger screens
+            brandLogo.innerHTML = '@Endrit_Vorfaj:~$ <span class="blinking-cursor"> | </span>';
+        }
+    }
+
+    // Initial adjustment on page load
+    adjustBrandLogoText();
+
+    // Adjust the text on window resize events
+    window.addEventListener('resize', adjustBrandLogoText);
+});

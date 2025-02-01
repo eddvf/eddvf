6ecbc74419ab62a1ab160ba7abe0c704aa3237af
eddvf <endrit.vorfaj@hotmail.com>
2025-02-01 20:38:24 +0100

    feb 2025

--- index.js
+++ index.js
@@ -23,3 +23,52 @@ document.addEventListener('DOMContentLoaded', function() {
     // Adjust the text on window resize events
     window.addEventListener('resize', adjustBrandLogoText);
 });
+
+// Add smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+
+// Add typewriter effect to header
+const headerText = "@Endrit_Vorfaj:~$ ";
+let currentChar = 0;
+const brandLogo = document.querySelector('.brand-logo');
+
+function typeWriter() {
+  if (currentChar < headerText.length) {
+    brandLogo.innerHTML = headerText.substring(0, currentChar + 1) + '<span class="blinking-cursor">|</span>';
+    currentChar++;
+    setTimeout(typeWriter, 100);
+  }
+}
+
+// Start the typewriter effect after page load
+window.addEventListener('load', () => {
+  brandLogo.innerHTML = '';
+  typeWriter();
+});
+
+
+
+// Back to top button
+window.addEventListener('scroll', function() {
+    const backToTop = document.getElementById('back-to-top');
+    if (window.scrollY > 300) {
+      backToTop.style.display = 'block';
+    } else {
+      backToTop.style.display = 'none';
+    }
+  });
+  
+  document.getElementById('back-to-top').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
